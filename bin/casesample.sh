8e28b7d80899252f1be80d1c401bc2bb7fa1349a
sk4lf <skalf.ak@gmail.com>
2019-01-17 15:58:09 +0200

    Case Statement example script added

---
+++ bin/casesample.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# demo of the case statement
+
+clear
+
+echo "MAIN MENU"
+echo "========="
+echo "1) Choice One"
+echo "2) Choice Two"
+echo "3) Choice Three"
+echo ""
+echo "Enter Choice: "
+read MENUCHOICE
+
+case $MENUCHOICE in
+  1)
+    echo "Contratulations for Choosinf the First Option";;
+  2)
+    echo "Choice 2 Chosen";;
+  3)
+    echo "Last Choice Made";;
+  *)
+    echo "You shose unwisely";;
+esac 
